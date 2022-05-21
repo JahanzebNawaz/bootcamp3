2b7e41d87586dcce7cce7b92f70d51e001985fd7
jahanzebNawaz <jahanzaib.ch9996@gmail.com>
2022-05-21 15:16:07 +0200

    VARIABLES.

---
+++ Python/code.py
@@ -0,0 +1,17 @@
+""" THIS IS THE PIECE OF CODE.
+
+ THIS 
+ THIS
+ """
+
+# THIS IS COMMENT print('THIS')
+# THIS IS COMMENT print('THIS')
+# THIS IS COMMENT print('THIS')
+# THIS IS COMMENT print('THIS')
+
+
+if 2 > 2:
+    print("Hello!") # this line prints hello.
+    print("World!")
+else:
+    print('THIS')
